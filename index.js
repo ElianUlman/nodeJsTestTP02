f01e7f2fb8f7b1e60d52fe8aa1cf67936412b2ef
LUCA GAMAS DE MAIO <49008593@ort>
2026-03-27 11:42:06 -0300

    minimal changes in ex2 redaction

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-//1
-let honorario="Señor"
-let apellido="Dickinson"
-
-console.log(`Buenos Dias ${honorario} ${apellido}`)
-
 //2
 import {sumar, restar, multiplicar, calculateCircleArea, PI} from "./src/modules/matematica.js"
 console.log(sumar(2,4))
 console.log(restar(1,10))
 console.log(multiplicar(2,4))
-console.log(`Area de circulo ${calculateCircleArea(2)}`)
+console.log(`Area de circulo ${calculateCircleArea(2)} con PI=${PI}`)
+
+//7
+import countryCurrencyMap from "country-currency-map";
+
