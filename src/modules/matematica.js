1f8f6f74c7e78eb8f693973003ce3ff932fea910
LUCA GAMAS DE MAIO <49008593@ort>
2026-03-27 11:35:45 -0300

    alternative verision for excercise 1 & 2

---
+++ src/modules/matematica.js
@@ -0,0 +1,16 @@
+
+const sumar = (a, b) =>  {return a+b}
+
+const restar = (a, b) =>  {return a-b}
+
+function multiplicar(a,b){
+    return a*b
+}
+
+function calculateCircleArea(radius){
+    return (radius*radius*PI)
+}
+
+const PI = 3.14159265359
+
+export {sumar, restar, multiplicar, calculateCircleArea, PI}
